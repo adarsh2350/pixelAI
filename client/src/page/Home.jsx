2641c4202a91d4aa1aae8b33ec40e4a38d3dafaa
Adarsh kumar <adarsh2350i@gmail.com>
2023-08-31 17:48:16 +0530

    Update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://pixel-ai-tcbs.onrender.com/api/v1/post', {
+      const response = await fetch('https://pixelai-f0ti.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
